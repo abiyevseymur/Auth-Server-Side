9b7397e221a58ab09ec9f7229245aea70f57a3e7
Seymur Abiyev <s.abiyev@asan.gov.az>
2020-02-19 17:29:09 +0400

    completed Auth node.js

--- services/passport.js
+++ services/passport.js
@@ -3,44 +3,43 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const LocalStrategy  = require('passport-local');
+const LocalStrategy = require('passport-local');
 
 //create local strategy
-const localOptions = {usernameField:'email'};
-const localLogin = new LocalStrategy(localOptions,function(email,password,done){
-    //verify username and pass if correct call done
-    User.findOne({email:email},function(err,user){
-        if(err){return done(err);}
-        if(!user){return done(null,false)}
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+        //verify username and pass if correct call done
+        User.findOne({ email: email }, function(err, user) {
+            if (err) { return done(err); }
+            if (!user) { return done(null, false) }
 
-        //compare passwords is equal 'password' to user.password?
-        user.comparePassword(password,function(err,isMatch){
-            if(err){return done(err);}
-            if(!isMatch){return done(nul,false);}
-            
-            return done(null,user)
+            //compare passwords is equal 'password' to user.password?
+            user.comparePassword(password, function(err, isMatch) {
+                if (err) { return done(err); }
+                if (!isMatch) { return done(null, false); }
+
+                return done(null, user)
+            })
         })
+
     })
-    
-})
-//setup jwt stategy
-const jwtOptions={
-    jwtFromRequest:ExtractJwt.fromHeader('authorization'),
+    //setup jwt stategy
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     secretOrKey: config.secret
 };
 
 
 // /create jwt Strategy
-const jwtLogin = new JwtStrategy(jwtOptions,function(payload,done){
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
     //see user id in payload exists i db
     //otherwise call done
-    User.findById(payload.sub,function(err,user){
-        if(err){return done(err,false);}
-        if(user){
-            done(null,user);
-        }
-        else{
-            done(null,false);
+    User.findById(payload.sub, function(err, user) {
+        if (err) { return done(err, false); }
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
         }
     })
 })
